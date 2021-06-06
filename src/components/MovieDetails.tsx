7801cb6f1de8c4b4b3b1fa9cb6bd87d459b9209a
Oleg Baras <tbaras@gmail.com>
2021-06-06 14:09:20 +0100

    extra-details

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -6,11 +6,14 @@ import { ImFilm } from "react-icons/im";
 const MovieDetails = ({ movie }: { movie: Movie }) => {
   return (
     <div className="movie-details">
+      <div className="extra-details">
+        <p>Year : {movie.Year}</p>
+        <p>Type : {movie.Type}</p>
+      </div>
       <div className="image">
         <img src={movie.Poster} alt="poster" />
       </div>
       <div className="description">
-        <p>{movie.Year}</p>
         <p>
           <ImFilm />
         </p>
